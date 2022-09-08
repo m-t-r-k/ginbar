575bd28df88f8953f6cf081242c4208c733a550f
Kaiser Maximilian <mk303@hdm-stuttgart.de>
2022-09-08 10:33:22 +0200

    Removed hard coded data from Imprint

--- client/src/components/Pages/Imprint/Imprint.js
+++ client/src/components/Pages/Imprint/Imprint.js
@@ -3,67 +3,34 @@ import { useEffect } from "react";
 import HeadlineTextGradientBg from "../../HeadlineTextGradientBg/HeadlineTextGradientBg";
 import MotionWrapper from "../../MotionWrapper";
 import './Imprint.scss';
+import WebsiteDate from '../../../data/website-data.json';
+import parse from 'html-react-parser';
 
 const Imprint = () => {
-  
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+	
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [])
+
+	const imprintGeneral = WebsiteDate.imprint.general;
+	const imprintDisclaimer = WebsiteDate.imprint.disclaimer;
 
   return(
+	
     <MotionWrapper>
 		<section className='pageWrapper'>
-			<HeadlineTextGradientBg image={`imprint_image.png`}>
-				<h1>Impressum</h1>
-					<div class='impressum'>
+			<HeadlineTextGradientBg image={imprintGeneral.image}>
+				<h1>{imprintGeneral.headline}</h1>
+				<div className='impressum'>
 					<p>
-						Angaben gemäß § 5 TMG<br></br>
-						<br></br>
-						Maximilian Kaiser<br></br> 
-						Schlüsselwiesen 29<br></br>
-						70186 Stuttgart <br></br>
-						<br></br>
-						<strong>Vertreten durch:</strong><br></br>
-						Maximilian Kaiser<br></br>
-						Annika Strauß<br></br>
-						<br></br>
-						<strong>Kontakt:</strong><br></br>
-						Telefon: +49 176 
-						<span className="hidden">84</span>
-						3221
-						<span className="hidden">0</span>
-						&#51;&#52;&#52;
-						<span className="hidden">1</span>
-						0
-						<br></br>
-						E-Mail: kaiser.
-						<span className="hidden">RemoveME</span>
-						maxi
-						<span className="hidden">NOBOTS</span>
-						&#64;live&#46;de
-						<br></br>
+						{parse(imprintGeneral.text)}
 					</p>
-					<br></br>
-					<br></br>
-					<h2>Haftungsausschluss</h2>
-					<p>
-					<strong>Haftung für Inhalte</strong><br></br>
-					<br></br>
-					Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br></br>
-					<br></br>
-					<strong>Haftung für Links</strong><br></br>
-					<br></br>
-					Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.<br></br>
-					<br></br>
-					<strong>Urheberrecht</strong><br></br>
-					<br></br>
-					Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.<br></br>
-					<br></br>
-					<strong>Datenschutz</strong><br></br>
-					<br></br>
-					Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. <br></br>
-					Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. <br></br>
-					Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.<br></br>
+				</div>
+				<br></br>
+				<br></br>
+				<h2>{imprintDisclaimer.headline}</h2>
+				<div className='impressum'>
+					<p>{parse(imprintDisclaimer.text)}</p>
 					{/*
 					<br></br>
 					<strong>Google Analytics</strong><br></br>
@@ -74,7 +41,6 @@ const Imprint = () => {
 					Website Impressum erstellt durch <a href="https://www.impressum-generator.de">impressum-generator.de</a> von der <a href="https://www.kanzlei-hasselbach.de/" rel="nofollow">Kanzlei Hasselbach</a>
 					</p>
 					*/}
-					</p>
 				</div>
 			</HeadlineTextGradientBg>
 		</section>
