07f0bb19d51c1dc82327b336eb6e456367418461
Strauß Annika <as324@hdm-stuttgart.de>
2023-01-16 12:12:02 +0100

    removed caching images and added font swap

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,13 @@
 import './App.scss';
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import SplashScreen from './components/SplashScreen/SplashScreen';
 import AnimatedRoutes from './components/AnimatedRouts';
-import GinData from './data/gin-data.json';
 
 const App = () => {
-  const [loaded, setLoaded] = useState(false);
-
-  const cacheImages = async (srcArray) => {  
-    const minTime = 4500;
-    const beginTime = Date.now();
-    const promises = await srcArray.map((src) => {
-      return new Promise(function (resolve, reject) {
-        const img = new Image();
-  
-        img.src = src;
-        img.onload = resolve();
-        img.onerror = reject();
-      });
-    });
-
-    await Promise.all(promises);
-
-    const currentTime = Date.now() - beginTime;
-    console.log(currentTime);
-    const waittime = currentTime < minTime ? minTime - currentTime : 0;
-    const timer = setTimeout(() => {
-      setLoaded(true);
-    }, waittime);
-
-    return () => clearTimeout(timer);
-  };
-
-  useEffect(() => {
-    const path = "../images/";
-
-    const imgs = GinData.map(gin => {
-      return [path + gin.imageBottle, path + gin.imageMoodPic, path + gin.imageMoodPicSmall];
-    }).flat();
-
-    cacheImages(imgs)
-  }, [])
-
   return (
-    <div className={loaded ? "App" : "App fixed"}>
+    <div className={"App"}>
       <Router>
         <Header></Header>
         <AnimatedRoutes></AnimatedRoutes>
