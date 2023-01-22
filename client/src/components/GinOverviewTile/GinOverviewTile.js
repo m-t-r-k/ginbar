91f1d6c7fa1eabd4711ed286dc1aa3f03aaae675
Strauß Annika <as324@hdm-stuttgart.de>
2023-01-22 15:50:04 +0100

    Revert "Revert "Lazy Loading via Intersection Observer""
    This reverts commit 38e1f8bbef94d0e9c87b8657c55bb14c3cb05998.

--- client/src/components/GinOverviewTile/GinOverviewTile.js
+++ client/src/components/GinOverviewTile/GinOverviewTile.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './GinOverviewTile.scss';
 import { Link } from "react-router-dom";
 import TagsList from "../TagsList/TagsList";
 
-class GinOverviewTile extends React.Component {
-  render() {
-    return (
-      <div key={this.props.id} className="ginOverviewTile">
-          <Link to={`/gins/${this.props.id}`}>
-            <div className='tileContent'>
-              <div id='recommendation' className={this.props.recommendation ? "show" : ""}>
-                <span>Tipp</span>
-              </div>
-              <span class="imageWrapper">
-                <img src={`../images/${this.props.imageBottle}`} alt={this.props.name} height="150px"></img>
-              </span>
-              <h2>{this.props.name}</h2>
-              <div className='tagsViewMoreWrapper'>
-                <hr></hr>
-                <span className='viewMore'>zum Gin</span>
-                <div className='tagsList'>
-                  <TagsList tags={this.props.tags} inverted={true}></TagsList>
-                </div>
+function GinOverviewTile (props) {
+  const [ isVisible, setIsVisible] = useState(false);
+  const containerRef = useRef(null)
+
+  const callbackFunction = (entries) => {
+    const [ entry ] = entries
+    if ( entry.isIntersecting ) setIsVisible(true)
+  }
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0
+    }
+    const refCurrent = containerRef.current
+    console.log(refCurrent)
+    const observer = new IntersectionObserver(callbackFunction, options)
+    if (refCurrent) observer.observe(refCurrent)
+
+    return () => {
+      if(refCurrent) observer.observe(refCurrent)
+    }
+  }, [containerRef])
+
+  return (
+    <div key={props.id} className="ginOverviewTile" ref={containerRef}>
+        <Link to={`/gins/${props.id}`}>
+          <div className='tileContent'>
+            <div id='recommendation' className={props.recommendation ? "show" : ""}>
+              <span>Tipp</span>
+            </div>
+            <span class="imageWrapper">
+            {isVisible ? <img src={`../images/${props.imageBottle}`} alt={props.name} height="150px"></img> : ""}
+            </span>
+            <h2>{props.name}</h2>
+            <div className='tagsViewMoreWrapper'>
+              <hr></hr>
+              <span className='viewMore'>zum Gin</span>
+              <div className='tagsList'>
+                <TagsList tags={props.tags} inverted={true}></TagsList>
               </div>
             </div>
-            <div className='moodPicBg' style={{backgroundImage: `url(../images/${this.props.imageMoodPicSmall})`}}></div>
-          </Link>
-      </div>
-    )
-  }
+          </div>
+          {isVisible ? <div className='moodPicBg' style={{backgroundImage: `url(../images/${props.imageMoodPicSmall})`}}></div>: ""}
+        </Link>
+    </div>
+  )
 }
 export default GinOverviewTile;
