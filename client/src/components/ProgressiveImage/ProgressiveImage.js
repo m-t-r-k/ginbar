caf7c998261bd57de380236d2b4b2ae5a3ea054b
Strauß Annika <as324@hdm-stuttgart.de>
2023-01-22 19:13:56 +0100

    removeThumbnails

--- client/src/components/ProgressiveImage/ProgressiveImage.js
+++ client/src/components/ProgressiveImage/ProgressiveImage.js
@@ -27,7 +27,7 @@ function ProgessiveImage (props) {
 
     return (
         <div id={props.source} className={isLoaded ? `${classes} loaded` : classes} style={{height: `${props.height}`}}>
-            <img className="blurryImage" src={`../images/blurry/thumb_${props.source}`} alt={props.alt} height="100%"  loading={props.lazy ? 'lazy' : 'eager'}></img>
+            <div className="blurryImage"  height="100%"  style={{backgroundColor: 'grey'}}></div>
         </div>
     )
 }
