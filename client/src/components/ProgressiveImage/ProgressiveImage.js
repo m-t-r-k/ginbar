1bed62b05b00227061a59b07e56519c9bf221aa5
Strauß Annika <as324@hdm-stuttgart.de>
2023-01-22 16:53:25 +0100

    intersectioObserver

--- client/src/components/ProgressiveImage/ProgressiveImage.js
+++ client/src/components/ProgressiveImage/ProgressiveImage.js
@@ -1,33 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './ProgressiveImage.scss';
 
 function ProgessiveImage (props) {
-    const [ isLoaded, setIsLoaded] = useState(false);
     const baseClass = "progressiveImage";
     let classes = `${baseClass} ${props.class}`;
     classes = props.isBG ? classes : classes + " not-bg-image";
 
-    useEffect(() => {
-        if (props.source) {
-            const baseElement = document.getElementById(props.source);
-            const url = `../images/${props.source}`
-            const handleLoad = () => {
-                setIsLoaded(true);
-                baseElement.style.backgroundImage = `url(${url})`;
-            };
-            const image = new Image();
-            image.addEventListener('load', handleLoad);
-            image.src = url;
-            return () => {
-                image.removeEventListener('load', handleLoad);
-            };
-        }
-    }, [props.source]);
-
-
     return (
-        <div id={props.source} className={isLoaded ? `${classes} loaded` : classes} style={{height: `${props.height}`}}>
-            <img className="blurryImage" src={`../images/blurry/thumb_${props.source}`} alt={props.alt} height="100%"></img>
+        <div id={props.source} className={classes} style={{backgroundImage: `url(../images/${props.source})`, height: `${props.height}`}}>
         </div>
     )
 }
