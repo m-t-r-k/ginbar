82c578449ee90c2c7ed5073ad5dad961ad40e266
Strauß Annika <as324@hdm-stuttgart.de>
2023-01-30 12:54:34 +0100

    first load bg element when in viewport

--- client/src/components/GinOverviewTile/GinOverviewTile.js
+++ client/src/components/GinOverviewTile/GinOverviewTile.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './GinOverviewTile.scss';
 import { Link } from "react-router-dom";
 import TagsList from "../TagsList/TagsList";
 
 function GinOverviewTile (props) {
+  const [ isVisible, setIsVisible] = useState(!props.lazy);
+  const containerRef = useRef(null)
+
+  const callbackFunction = (entries) => {
+    const [ entry ] = entries
+    if ( entry.isIntersecting ) setIsVisible(true)
+  }
+
+  useEffect(() => {
+    if(!isVisible) {
+      const options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0
+      }
+      const refCurrent = containerRef.current
+      const observer = new IntersectionObserver(callbackFunction, options)
+      if (refCurrent) observer.observe(refCurrent)
+
+      return () => {
+        if(refCurrent) observer.unobserve(refCurrent)
+      }
+    }
+  }, [containerRef, isVisible])
+
   return (
-    <div key={props.id} className="ginOverviewTile">
+    <div key={props.id} className="ginOverviewTile" ref={containerRef}>
         <Link to={`/gins/${props.id}`}>
           <div className='tileContent'>
             <div id='recommendation' className={props.recommendation ? "show" : ""}>
@@ -23,7 +48,7 @@ function GinOverviewTile (props) {
               </div>
             </div>
           </div>
-          <div className='moodPicBg' style={{backgroundImage: `url(../images/${props.imageMoodPicSmall})`}}></div>
+          {(isVisible || props.lazy) ? <div className='moodPicBg' style={{backgroundImage: `url(../images/${this.props.imageMoodPicSmall})`}}></div> : ""}
         </Link>
     </div>
   )
