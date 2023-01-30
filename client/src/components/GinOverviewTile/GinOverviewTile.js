3ef89f100daa4b60ca0f41d505c500869c4ebe00
Strauß Annika <as324@hdm-stuttgart.de>
2023-01-30 12:49:30 +0100

    it lazy again

--- client/src/components/GinOverviewTile/GinOverviewTile.js
+++ client/src/components/GinOverviewTile/GinOverviewTile.js
@@ -12,7 +12,7 @@ function GinOverviewTile (props) {
               <span>Tipp</span>
             </div>
             <span className="imageWrapper">
-              <img src={`../images/${props.imageBottle}`} alt={props.name} height="150px"></img>
+              <img src={`../images/${props.imageBottle}`} alt={props.name} height="150px" loading={props.lazy ? 'lazy' : 'eager'}></img>
             </span>
             <h2>{props.name}</h2>
             <div className='tagsViewMoreWrapper'>
