fd45347977c50ee54ecd488aea4d309304d2a7b6
Strauß Annika <as324@hdm-stuttgart.de>
2023-01-30 16:22:21 +0100

    Test PI0: blend-in Effect mit timeout

--- client/src/components/BGImage/BGImage.js
+++ client/src/components/BGImage/BGImage.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './BGImage.scss';
 
 function BGImage (props) {
+    const [ loading, setLoading] = useState("");
+
+    useEffect(() => {
+        console.log(loading)
+        const timeout = setTimeout(() => {
+            setLoading(" loaded");
+        }, 10)
+      
+        return () => clearTimeout(timeout)
+        
+    }, [])
+
     return (
-        <div className='moodPicBg' style={{backgroundImage: `url(../images/${props.imageMoodPicSmall})`}}></div>
+        <div className={`moodPicBg${loading}`} style={{backgroundImage: `url(../images/${props.imageMoodPicSmall})`}}></div>
     )
 }
 export default BGImage;
