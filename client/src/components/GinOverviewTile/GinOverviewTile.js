8324d9d12d672ac1f8dc8e8d3d5a3e1901927125
Strauß Annika <as324@hdm-stuttgart.de>
2023-02-03 16:59:07 +0100

    Revert "aspect-ratio on bottles"
    This reverts commit 9d841e0ba6b46be872cf2c2fef53dd8719c2e02b.

--- client/src/components/GinOverviewTile/GinOverviewTile.js
+++ client/src/components/GinOverviewTile/GinOverviewTile.js
@@ -1,31 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './GinOverviewTile.scss';
 import { Link } from "react-router-dom";
 import TagsList from "../TagsList/TagsList";
+import BGImage from '../BGImage/BGImage';
+import ProgessiveImage from '../ProgressiveImage/ProgressiveImage';
 
-class GinOverviewTile extends React.Component {
-  render() {
-    return (
-      <div key={this.props.id} className="ginOverviewTile">
-          <Link to={`/gins/${this.props.id}`}>
-            <div className='tileContent'>
-              <div id='recommendation' className={this.props.recommendation ? "show" : ""}>
-                <span>Tipp</span>
-              </div>
-              <img src={`../images/${this.props.imageBottle}`} alt={this.props.name}></img>
-              <h2>{this.props.name}</h2>
-              <div className='tagsViewMoreWrapper'>
-                <hr></hr>
-                <span className='viewMore'>zum Gin</span>
-                <div className='tagsList'>
-                  <TagsList tags={this.props.tags} inverted={true}></TagsList>
-                </div>
+function GinOverviewTile (props) {
+  const [ isVisible, setIsVisible] = useState(!props.lazy);
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    const callbackFunction = (entries) => {
+      const [ entry ] = entries
+      if ( entry.isIntersecting ) {
+        setIsVisible(true);
+      }
+    }
+  
+    if(!isVisible) {
+      const options = {
+        root: null,
+        rootMargin: "0px 0px 300px 0px",
+        threshold: 0
+      }
+      const refCurrent = containerRef.current
+      const observer = new IntersectionObserver(callbackFunction, options)
+      if (refCurrent) observer.observe(refCurrent)
+
+      return () => {
+        if(refCurrent) observer.unobserve(refCurrent)
+      }
+    }
+  }, [containerRef, isVisible])
+
+  return (
+    <div key={props.id} className="ginOverviewTile" ref={containerRef}>
+        <Link to={`/gins/${props.id}`}>
+          <div className='tileContent'>
+            <div id='recommendation' className={props.recommendation ? "show" : ""}>
+              <span>Tipp</span>
+            </div>
+            <span className="imageWrapper">
+              {!props.lazy ? <ProgessiveImage source={props.imageBottle} alt={props.name} height="150px"></ProgessiveImage> : ""}
+              {props.lazy && isVisible ? <ProgessiveImage source={props.imageBottle} alt={props.name} height="150px"></ProgessiveImage> : ""}
+            </span>
+            <h2>{props.name}</h2>
+            <div className='tagsViewMoreWrapper'>
+              <hr></hr>
+              <span className='viewMore'>zum Gin</span>
+              <div className='tagsList'>
+                <TagsList tags={props.tags} inverted={true}></TagsList>
               </div>
             </div>
-            <div className='moodPicBg' style={{backgroundImage: `url(../images/${this.props.imageMoodPicSmall})`}}></div>
-          </Link>
-      </div>
-    )
-  }
+          </div>
+          {!props.lazy ? <BGImage source={props.imageMoodPicSmall}></BGImage> : ""}
+          {props.lazy && isVisible ? <BGImage source={props.imageMoodPicSmall}></BGImage> : ""}
+        </Link>
+    </div>
+  )
 }
 export default GinOverviewTile;
